9aa05c85c18c534fdfbd0b42c46e6892a8bfe36c
Ghatan55 <ghatanhardanni@gmail.com>
2025-05-15 10:42:10 +0700

    Melanjutkan step 3,4 dan 5

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -112,5 +112,17 @@ private:
                 start->prev = NULL; // Step 2b: Start.prev = NULL
         }
         else
+        {
+            // Step 3: Link previous node to next of current
+            current->prev->next = current->next;
+
+            // Step 4: if current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
+
+        // Step 5: Delete the node
+        delete current;
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
 };
