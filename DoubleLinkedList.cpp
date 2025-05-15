b08c58649867f9d3bb37d4873fcc79bbb02fe09a
Ghatan55 <ghatanhardanni@gmail.com>
2025-05-15 10:06:04 +0700

    Menambahkan langkah langkah di dalam fungsi void addNode

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -27,5 +27,32 @@ private:
         string nm;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+
+        // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data fields
+        newNode->noMhs = nim;
+
+        // Step 3: Insert at beginning if list is empty or nim is smallest
+        if (start == NULL || nim <= start->noMhs)
+        {
+            cout << "\nDuplicate number not allowed" << endl;
+            return;
+        }
+
+        // Step 4: newNode.next = Start
+        newNode->next = start;
+
+        // step 5: Start.prev = newNode (if Start exists)
+        if (start != NULL)
+            start->prev = newNode;
+
+        // Step 6: newNode.prev = NULL
+        newNode->prev = NULL;
+
+        // Step 7: Start = newNode
+        start = newNode;
+        return;
     };
 };
